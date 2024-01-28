59a59cb17ee691d56b1fef6e7543807e00b2b2a0
raresElu12 <grosanrares@yahoo.com>
2024-01-28 15:11:53 +0100

    new files

---
+++ public/main.js
@@ -0,0 +1,24 @@
+function time(){
+    date = new Date();
+    document.getElementById("datetime").innerHTML = date.toLocaleTimeString("fr-fr");
+
+} setInterval(time,1000);
+
+function carousel(input){
+    if(input === 0){
+        a = document.getElementById("body-container");
+        a.setAttribute("style","left:-100vw");
+        document.getElementById("first-arrow").style.zIndex="-1";
+        document.getElementById("first-arrow").style.visibility = "hidden";
+        document.getElementById("second-arrow").style.visibility="visible";
+        document.getElementById("second-arrow").style.zIndex="1";
+    }
+    else if(input === 1){
+        document.getElementById("body-container").setAttribute("style", "right:-100vw");
+        document.getElementById("second-arrow").style.zIndex="-1";
+        document.getElementById("second-arrow").style.visibility="hidden";
+        document.getElementById("first-arrow").style.visibility="visible";
+        document.getElementById("first-arrow").style.zIndex="1";
+    }
+}
+
