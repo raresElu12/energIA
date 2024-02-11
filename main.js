360145eb1afaefad8583c33a5335dcf20692d9d9
raresElu12 <grosanrares@yahoo.com>
2024-02-11 16:54:14 +0100

    coommmmmmmmmmiiiiiiiiiiiiiit

--- main.js
+++ main.js
@@ -32,6 +32,42 @@ document.getElementById("link4").addEventListener("click", () => incrementViewCo
 document.getElementById("link5").addEventListener("click", () => incrementViewCount("link5"));
 document.getElementById("link6").addEventListener("click", () => incrementViewCount("link6"));
 
-function renderPopularNews(){
-    let a = document.body.innerHTML;
+
+
+
+
+async function fetchViewCounts(num_art){
+    let list = []
+    for(let i = 1;i<num_art+1;i++){
+
+    try { 
+        let response = await fetch(`https://almond-understood-makeup.glitch.me/getViewCount/link${i}`);
+        await response.json().then(res => list.push({'article' : i, 'viewCount' : res['count']}), err => console.error(err));
+    } catch (error){
+        console.error(error);
+    }
 }
+    return list.sort((a,b) => b['viewCount'] - a['viewCount'])
+}
+
+function renderPopularNews(num_art){
+    let a = document.body.innerHTML;
+    listeArticles = fetchViewCounts(num_art);
+    b = a.substring(0,a.length-1121);
+    b += `<br><br><br><br><br><br>\n
+    <h2>Nouvelles Populaires</h2>\n
+    <div id="body-container-2">\n
+        <div class="full-article-container">\n`;  
+    for (let i =1;i<num_art/2;i++){
+        b+= `<a href="articles/art-${listeArticles[i-1]}" target="_blank" id="link${6+listeArticles[i-1]}">\n
+        <div class="article-box">\n<img src="photos/art-${listeArticles[i-1]}.jpg">\n<br><br><br>\n<p>${document.getElementById(`titre-art-${listeArticles[i-1]}`)}</p>\n</div>\n</a>`;
+    }
+    b+=`\n<div class="full-article-container">`;
+    for(let i = num_art/2; i < num_art;i++){
+        b+= `<a href="articles/art-${listeArticles[i-1]}" target="_blank" id="link${6+listeArticles[i-1]}">\n
+        <div class="article-box">\n<img src="photos/art-${listeArticles[i-1]}.jpg">\n<br><br><br>\n<p>${document.getElementById(`titre-art-${listeArticles[i-1]}`)}</p>\n</div>\n</a>`;
+    }
+    b+=`\n</div>\n</div>\n<br><br><br><br>`;
+}
+
+renderPopularNews(6);
